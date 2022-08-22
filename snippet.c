2ef04c6f1fbf3b57204e49bb436f6d9502910692
Shweta2003 <65994349+Shweta2003@users.noreply.github.com>
2022-08-22 21:06:01 +0530

    Create snippet.c

---
+++ snippet.c
@@ -0,0 +1,11 @@
+"bare minimum C program":{
+  "prefix":"main2",
+  "body":[
+      "#include ,stdio.h>",
+      "int main()";
+      "{",
+      "\treturn 0;",
+      "}"
+    ],
+    "description": "C program"
+  }
